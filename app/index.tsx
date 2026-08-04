0c0a6133eb67aeaee2aa8a2f6d82b5e247d2cdba
Fransun7 <fransunlight777@gmail.com>
2026-08-04 18:41:44 +0100

    installed Nativewind while setting up ReactNative

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-red-500 text-2xl font-bold">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
