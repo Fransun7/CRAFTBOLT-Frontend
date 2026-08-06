247423f0aa78709605f2ff9ec664627a8d386306
OloyeBright <oloyebrightoluwasegunota.com>
2026-08-06 20:53:39 +0100

    Update index.tsx with test inputs

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,16 @@
+// import { Text, View } from "react-native";
+
+// export default function Index() {
+//   return (
+//     <View className="flex-1 items-center justify-center bg-white">
+//       <Text className="text-red-500 text-2xl font-bold">
+//         Edit app/index.tsx to edit this screen.
+//       </Text>
+//     </View>
+//   );
+// }
+
+
 import CustomInput from "@/components/CustomInput/CustomInput";
 import { useState } from "react";
 import { ScrollView, View } from "react-native";
