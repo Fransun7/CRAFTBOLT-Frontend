e7e7a56ae6192c389bbcfac32fed8855563dee64
Fransun7 <fransunlight777@gmail.com>
2026-08-11 15:02:02 +0100

    custom-input

--- app/index.tsx
+++ app/index.tsx
@@ -6,12 +6,29 @@ export default function Index() {
   const [phone, setPhone] = useState("");
 
   return (
-    <View className="flex-1 justify-center">
+    <View className="flex-1 justify-center gap-10  border border-2 p-10 bg-slate-400">
       <CustomInput
         label="Phone Number"
         placeholder="Enter Your phone number"
         value={phone}
         onChangeText={setPhone}
+      />
+
+      <CustomInput
+        label="Full Name"
+        placeholder="Enter Your Full Name"
+        value={phone}
+        onChangeText={setPhone}
+        disabled={true}
+      />
+
+      <CustomInput
+        label="Email"
+        placeholder="Enter Your Email"
+        value={phone}
+        onChangeText={setPhone}
+        error={true}
+        errorMessage="Please enter your email"
       />
     </View>
   );
--- components/inputs/CustomInput.tsx
+++ components/inputs/CustomInput.tsx
@@ -1,5 +1,6 @@
 import { Colors } from "@/design-system/colors";
 import { Typography } from "@/design-system/typography";
+import { useState } from "react";
 import { Text, TextInput, View } from "react-native";
 
 // I NEED AN INTERFACE FOR CUSTOMINPUT COMPONENT
@@ -7,8 +8,9 @@ interface CustomInputProps {
   label?: string;
   placeholder: string;
   value: string;
-  error?: string;
-  disable?: boolean;
+  error?: boolean;
+  errorMessage?: string;
+  disabled?: boolean;
   onChangeText: (text: string) => void;
 }
 
@@ -17,9 +19,13 @@ function CustomInput({
   placeholder,
   value,
   error,
-  disable,
+  errorMessage,
+  disabled,
   onChangeText,
 }: CustomInputProps) {
+  // I NEED A STATE FOR THE FOCUS OF THE INPUT FIELD
+  const [isFocused, setIsFocused] = useState(false);
+
   return (
     // I NEED THE COMPONENT TO DISPLAYS LABEL IF ONLY IT EXIST
     <View>
@@ -29,7 +35,7 @@ function CustomInput({
             Typography.bodySmall,
             {
               color: Colors.surface.textPrimary,
-              backgroundColor: Colors.surface.surface,
+              // backgroundColor: Colors.surface.surface,
             },
           ]}
           className="mb-2"
@@ -41,13 +47,39 @@ function CustomInput({
         placeholder={placeholder}
         value={value}
         onChangeText={onChangeText}
-        className="h-12 rounded-lg border px-4 text-base"
+        placeholderTextColor={Colors.surface.textMuted}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
+        editable={!disabled}
+        className="h-12 rounded-lg border px-4"
         style={{
-          borderColor: Colors.surface.border,
-          fontWeight: Typography.bodyRegular.fontWeight,
-          color: Colors.status.error,
+          borderColor: disabled
+            ? Colors.surface.border
+            : error
+              ? Colors.status.error
+              : isFocused
+                ? Colors.brand.primary
+                : Colors.surface.border,
+          ...Typography.bodyRegular,
+          color: disabled
+            ? Colors.surface.textMuted
+            : Colors.surface.textPrimary,
+          backgroundColor: disabled
+            ? Colors.surface.background
+            : Colors.surface.surface,
         }}
       />
+      {error && errorMessage && (
+        <Text
+          style={{
+            ...Typography.bodySmall,
+            color: Colors.status.error,
+          }}
+          className="mt-1"
+        >
+          {errorMessage}
+        </Text>
+      )}
     </View>
   );
 }
