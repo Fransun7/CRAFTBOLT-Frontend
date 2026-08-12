8311477726a0ccad1472bd26ad90a15b9fb520f3
Fransun7 <fransunlight777@gmail.com>
2026-08-12 12:24:46 +0100

    called the loading spinner component to index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,13 @@
+import LoadingSpinner from "@/components/LoadingSpinner/LoadingSpinner";
 import { useState } from "react";
 import { View } from "react-native";
-import CustomInput from "../components/inputs/CustomInput";
 
 export default function Index() {
   const [phone, setPhone] = useState("");
 
   return (
     <View className="flex-1 justify-center">
-      <CustomInput
-        label="Phone Number"
-        placeholder="Enter Your phone number"
-        value={phone}
-        onChangeText={setPhone}
-      />
+      <LoadingSpinner />
     </View>
   );
 }
